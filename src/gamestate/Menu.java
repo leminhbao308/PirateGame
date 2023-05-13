23598c0d95e405d738758277e31a9a2d2d95586b
leminhbao308 <besilent308@gmail.com>
2023-05-14 03:01:22 +0700

    Added background image

--- src/gamestate/Menu.java
+++ src/gamestate/Menu.java
@@ -11,100 +11,102 @@ import java.awt.image.BufferedImage;
 
 public class Menu extends State implements Statemethods {
 
-	private MenuButton[] buttons = new MenuButton[3];
-	private BufferedImage backgroundImage;
-	private int menuX, menuY, menuWidth, menuHeight;
+    private MenuButton[] buttons = new MenuButton[3];
+    private BufferedImage backgroundImage, backgroundImagePink;
+    private int menuX, menuY, menuWidth, menuHeight;
 
-	public Menu(Game game) {
-		super(game);
-		loadButtons();
-		loadBackground();
-	}
+    public Menu(Game game) {
+        super(game);
+        loadButtons();
+        loadBackground();
+        backgroundImagePink = LoadSave.GetSpriteAtlas(LoadSave.MENU_BACKGROUND_IMG);
+    }
 
-	private void loadBackground() {
-		backgroundImage = LoadSave.GetSpriteAtlas(LoadSave.MENU_BACKGROUND);
-		menuWidth = (int) (backgroundImage.getWidth() * Game.SCALE);
-		menuHeight = (int) (backgroundImage.getHeight() * Game.SCALE);
-		menuX = Game.GAME_WIDTH / 2 - menuWidth / 2;
-		menuY = (int) (45 * Game.SCALE);
-	}
+    private void loadBackground() {
+        backgroundImage = LoadSave.GetSpriteAtlas(LoadSave.MENU_BACKGROUND);
+        menuWidth = (int) (backgroundImage.getWidth() * Game.SCALE);
+        menuHeight = (int) (backgroundImage.getHeight() * Game.SCALE);
+        menuX = Game.GAME_WIDTH / 2 - menuWidth / 2;
+        menuY = (int) (45 * Game.SCALE);
+    }
 
-	private void loadButtons() {
-		buttons[0] = new MenuButton(Game.GAME_WIDTH / 2, (int) (150 * Game.SCALE), 0, Gamestate.PLAYING);
-		buttons[1] = new MenuButton(Game.GAME_WIDTH / 2, (int) (220 * Game.SCALE), 1, Gamestate.OPTIONS);
-		buttons[2] = new MenuButton(Game.GAME_WIDTH / 2, (int) (290 * Game.SCALE), 2, Gamestate.QUIT);
-	}
+    private void loadButtons() {
+        buttons[0] = new MenuButton(Game.GAME_WIDTH / 2, (int) (150 * Game.SCALE), 0, Gamestate.PLAYING);
+        buttons[1] = new MenuButton(Game.GAME_WIDTH / 2, (int) (220 * Game.SCALE), 1, Gamestate.OPTIONS);
+        buttons[2] = new MenuButton(Game.GAME_WIDTH / 2, (int) (290 * Game.SCALE), 2, Gamestate.QUIT);
+    }
 
-	@Override
-	public void update() {
-		for (MenuButton mb : buttons)
-			mb.update();
-	}
+    @Override
+    public void update() {
+        for (MenuButton mb : buttons)
+            mb.update();
+    }
 
-	@Override
-	public void draw(Graphics g) {
+    @Override
+    public void draw(Graphics g) {
 
-		g.drawImage(backgroundImage, menuX, menuY, menuWidth, menuHeight, null);
+        g.drawImage(backgroundImagePink, 0, 0, Game.GAME_WIDTH, Game.GAME_HEIGHT, null);
+        g.drawImage(backgroundImage, menuX, menuY, menuWidth, menuHeight, null);
 
-		for (MenuButton mb : buttons)
-			mb.draw(g);
-	}
+        for (MenuButton mb : buttons)
+            mb.draw(g);
+    }
 
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
-	@Override
-	public void mousePressed(MouseEvent e) {
-		for (MenuButton mb : buttons) {
-			if (isIn(e, mb)) {
-				mb.setMousePressed(true);
-				break;
-			}
-		}
-	}
+    @Override
+    public void mousePressed(MouseEvent e) {
+        for (MenuButton mb : buttons) {
+            if (isIn(e, mb)) {
+                mb.setMousePressed(true);
+                break;
+            }
+        }
+    }
 
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		for (MenuButton mb : buttons) {
-			if (isIn(e, mb)) {
-				if (mb.isMousePressed())
-					mb.applyGameState();
-				break;
-			}
-		}
-		resetButtons();
-	}
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        for (MenuButton mb : buttons) {
+            if (isIn(e, mb)) {
+                if (mb.isMousePressed())
+                    mb.applyGameState();
+                break;
+            }
+        }
+        resetButtons();
+    }
 
-	private void resetButtons() {
-		for (MenuButton mb : buttons) {
-			mb.resetBoolean();
-		}
-	}
+    private void resetButtons() {
+        for (MenuButton mb : buttons) {
+            mb.resetBoolean();
+        }
+    }
 
-	@Override
-	public void mouseMoved(MouseEvent e) {
-		for (MenuButton mb : buttons)
-			mb.setMouseOver(false);
-		for (MenuButton mb : buttons)
-			if (isIn(e, mb)) {
-				mb.setMouseOver(true);
-				break;
-			}
-	}
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        for (MenuButton mb : buttons)
+            mb.setMouseOver(false);
+        for (MenuButton mb : buttons)
+            if (isIn(e, mb)) {
+                mb.setMouseOver(true);
+                break;
+            }
+    }
 
-	@Override
-	public void keyPressed(KeyEvent e) {
-		if (e.getKeyCode() == KeyEvent.VK_ENTER)
-			Gamestate.state = Gamestate.PLAYING;
-	}
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_ENTER)
+            Gamestate.state = Gamestate.PLAYING;
+    }
 
-	@Override
-	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
 }
--- src/utilz/LoadSave.java
+++ src/utilz/LoadSave.java
@@ -1,7 +1,5 @@
 package utilz;
 
-import main.Game;
-
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
@@ -12,13 +10,15 @@ public class LoadSave {
 
     public static final String PLAYER_ATLAS = "Character_Main.png";
     public static final String LEVEL_ATLAS = "Outside_Terrain.png";
-    public static final String LEVEL_ONE_DATA = "Level_one_data.png";
+    //    public static final String LEVEL_ONE_DATA = "Level_one_data.png";
+    public static final String LEVEL_ONE_DATA = "Level_one_data_long.png";
     public static final String MENU_BUTTONS = "Menu_button.png";
     public static final String MENU_BACKGROUND = "Menu_background.png";
     public static final String PAUSE_BACKGROUND = "Pause_menu.png";
     public static final String SOUND_BUTTONS = "Sound_button.png";
     public static final String URM_BUTTONS = "Urm_buttons.png";
     public static final String VOLUME_BUTTONS = "Volume_buttons.png";
+    public static final String MENU_BACKGROUND_IMG = "Background_menu.png";
 
     public static BufferedImage GetSpriteAtlas(String fileName) {
 
@@ -40,8 +40,8 @@ public class LoadSave {
     }
 
     public static int[][] GetLevelData() {
-        int[][] lvlData = new int[Game.TILES_IN_HEIGHT][Game.TILES_IN_WIDTH];
         BufferedImage img = GetSpriteAtlas(LEVEL_ONE_DATA);
+        int[][] lvlData = new int[img.getHeight()][img.getWidth()];
 
         for (int i = 0; i < img.getHeight(); i++) {
             for (int j = 0; j < img.getWidth(); j++) {
